d8d987658a120c011f6adf06ff21ed9473270fbf
Stephen Twigg <sdtwigg@eecs.berkeley.edu>
2015-04-22 02:31:53 -0700

    Adjust todo list

--- chiselfrontend/src/main/scala/switch.scala
+++ chiselfrontend/src/main/scala/switch.scala
@@ -3,12 +3,13 @@ package gama
 object switch {
   def apply(left: UIntLike)(ises: Seq[SwitchIs])(implicit em: EnclosingModule): Unit = {
     ises.foreach(is => {
-      when.create(left.do_eq(is.right, EnclosureInfo(em, None)), is.doWork, em) // TODO: TAKE INFO
+      when.create(left.do_eq(is.right, EnclosureInfo(em, None)), is.doWork, em)
     })
+    // TODO: CONSIDER: Should EnclosureInfo be totally filled in? How
   }
 }
 object is {
-  def apply(right: UIntLike)(work: =>Unit) = new SwitchIs(right, work)
+  def apply(right: UIntLike)(work: =>Unit): SwitchIs = new SwitchIs(right, work)
 }
 
 protected[gama] class SwitchIs(val right: UIntLike, work: =>Unit) {
