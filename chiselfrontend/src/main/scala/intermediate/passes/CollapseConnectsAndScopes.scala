618844f53d2304f8bc4de8f100c7c1c2ca71867f
Stephen Twigg <sdtwigg@eecs.berkeley.edu>
2015-05-16 03:18:29 -0700

    Elementary adjust to collapse connects and scopes

--- chiselfrontend/src/main/scala/intermediate/passes/CollapseConnectsAndScopes.scala
+++ chiselfrontend/src/main/scala/intermediate/passes/CollapseConnectsAndScopes.scala
@@ -9,23 +9,35 @@ object CollapseConnectsAndScopes extends GamaPass {
 
   sealed trait CollapseTarget
   case class CTSymbol(symbol: RefSymbol)
-  case class CTIO(path: IOPathTrace)
+  case class CTIO(module: ModuleRef, field: String) // Don't bother with IOPathTrace since this pass assumes flat IO
   case class CTMem(mem: MemDesc)
 
-  sealed trait ListedCmdHW
-  case class UnresolvedCmdHW(cmd: CmdHW)
-  case class ResolvedCmdHW(cmd: CmdHW)
+  sealed trait OpenCondition
+  case class OpenWhen(cond: ExprHW, parent: Option[OpenCondition]) extends OpenCondition
+  case class OpenElse(owhen: OpenWhen) extends OpenCondition
+  // Instead of updating the condition on all open journals, just sync accessed journal as new condition encountered
+  //   by closing old conditions to ancestor (and then opening up new ones until the current condition opened)
 
   def transform(target: ElaboratedModule): ElaboratedModule = {
-    def collapseBlock(cmds: Iterable[CmdHW], parentScope: ScopeContainer): CmdHW = ???
+    // TODO: Assumes InternalAggregateExplode, CircuitFlattenIO run (thus very minimal presence of aggregates)
+    //       Assumes ExpandM/VSelect run (no RefVSelect or RefMSelect exist in the tree)
+    def collapseBlock(cmds: Iterable[CmdHW], parentScope: Option[ScopeContainer]): Iterable[CmdHW] = {
+      val scope = new ScopeContainer(parentScope)
+      ???
+    }
     target
   }
 
   class ScopeContainer(parent: Option[ScopeContainer]) {
-    private val resolvedDecl = ListB.empty[CmdHW] // Emit first since connect, other cmds may use
-    private val otherCmds = ListB.empty[ListedCmdHW]    // Emit second (nested scopes go here)
-    private val resolvedConnect = ListB.empty[CmdHW]  // Emit last since use decl and maybe other cmds
+    private val resolvedCmds = ListB.empty[CmdHW]
+    private val scopedJournals = HMap.empty[CollapseTarget, ConnectJournal]
   }
 
-  class ConnectJournal
+  sealed abstract class ConnectJournal(scope: ScopeContainer)
+  final class WireJournal(target: CollapseTarget, scope: ScopeContainer) extends ConnectJournal(scope)
+    // translate when into muxes, needs defaults
+  final class RegJournal(target: CTSymbol, scope: ScopeContainer) extends ConnectJournal(scope)
+    // track the whens and just emit that when tree
+  final class MemJournal(mem: MemDesc, scope: ScopeContainer) extends ConnectJournal(scope)
+    // just tracks scope, when memread/memwrite encountered, immediately emit to scope
 }
